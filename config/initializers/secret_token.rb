960a4af65d665f7d14eefe4a26fe64329475ddd3
Ruchi Birla <ruchi.birla92@gmail.com>
2013-08-24 13:11:59 +0530

    crud for ideas complete

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ideas::Application.config.secret_key_base = 'e686a36dedb8041108f0352c934ac8397e1913e220c808be20e76dddf690e8e9e53798b4ceaee8fec2185c0cf58b88919d938c07972fe8cdc704a86da85b1722'
